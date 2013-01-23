a9865d78d786d9fedea41f4bd6ad745e84ee3eea
Antonius Denny Harijanto <adennyh@gmail.com>
2013-01-22 16:25:15 -0800

    Initial Commit

---
+++ testCV/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  testCV
+//
+//  Created by Antonius Harijanto on 1/22/13.
+//  Copyright (c) 2013 UW Mobile Accessibility. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
